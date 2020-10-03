c4dbc2f791d612d3a2f6e109855d083f440a1459
胡一文 <291605552@qq.com>
2020-10-03 19:21:49 +0800

    创建构造函数和ToString

--- loggingSystem/src/main/java/com/hu/gmall/pojo/Class.java
+++ loggingSystem/src/main/java/com/hu/gmall/pojo/Class.java
@@ -3,4 +3,33 @@ package com.hu.gmall.pojo;
 public class Class {
     private String name;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Class(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
